b166b8c1924ea737ccc49a8550a38f475bc269d9
Taha_Essaouir <154090928+Taha-Essaouir@users.noreply.github.com>
2024-11-04 20:50:34 +0100

    test

--- Eureka Server/Eureka Server/src/main/java/com/Eureka/Server/demo/EurekaServerApplication.java
+++ Eureka Server/Eureka Server/src/main/java/com/Eureka/Server/demo/EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication // Indique que c'est une application Spring Boot, activant l'auto-configuration et le scan des composants
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+//jjhsgggffffffffffffffff
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
