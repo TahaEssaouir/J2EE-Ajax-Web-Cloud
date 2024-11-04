07fb82a4a18e78371681c6c26f4c1ee8dbf93989
Taha_Essaouir <154090928+Taha-Essaouir@users.noreply.github.com>
2024-11-04 23:00:00 +0100

    Update ConfigSeverApplication.java

--- configSever/configSever/src/main/java/com/example/configSever/ConfigSeverApplication.java
+++ configSever/configSever/src/main/java/com/example/configSever/ConfigSeverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer 
 public class ConfigSeverApplication {
-//AAAAAA hhhhhhhhhhhh
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigSeverApplication.class, args);
 	}
